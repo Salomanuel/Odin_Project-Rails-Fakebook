197a8e1b149713162a52c0f86ed6d57cef24988f
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-03 22:16:06 +0200

    implement form for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 	def create
 		@user  = User.new(user_params)
 		if @user.save
-			render inline: "<h1>Good to go</h1>"
+			redirect_to @user
 		else
 			render inline: "<h1>Oh no</h1>"
 		end
