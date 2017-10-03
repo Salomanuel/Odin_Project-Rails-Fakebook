edbdf48a69231a70949b36d89f9eb5d87f3dafbc
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-03 12:45:00 +0200

    add test for Users index (skipped for now)

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,16 @@
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+	test "it should have an index page" do
+		skip
+		get users_path
+		assert_response :success
+		assert_select "title", "All Users"
+		assert_select "h1", "All Users"
+	end
+
+	test "it should have a profile page" do
+	end
+
+
+
+end
