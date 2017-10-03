2902ca6251eb077ffe7eb1b1f3ac47501fb248f7
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-03 19:16:31 +0200

    add test for a basic User page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,10 @@
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+	def setup
+		@user  = users(:manuel)
+		@user2 = users(:marion)
+	end
+
 	test "it should have an index page" do
 		get users_path
 		assert_response :success
@@ -8,6 +13,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "it should have a profile page" do
+		get user_path(@user)
+		assert_response :success
+		assert_select "title", "#{@user.name}"
+		assert_select "h1", "#{@user.name}"
 	end
 
 
