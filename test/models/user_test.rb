887bbb33fc93371b06693099e9aab029cbc75b3e
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-03 21:51:11 +0200

    add basic validation test for Users creation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = User.new(name: "Manuel")
+	end
+
+	test "it should be valid" do
+		assert @user.valid?
+	end
+
+	test "it should have a name, not too long" do
+		@user.name = "    "
+		assert_not @user.valid?
+		@user.name = "a" * 20
+		assert_not @user.valid?
+	end
 end
