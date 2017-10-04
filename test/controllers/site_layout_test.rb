7df7e13c450f0d59bd0010834b36fd93d5d6b20b
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-04 09:58:19 +0200

    write tests for sessions

--- test/controllers/site_layout_test.rb
+++ test/controllers/site_layout_test.rb
@@ -5,21 +5,28 @@ class BaseControllerTest < ActionDispatch::IntegrationTest
 	test "should get home" do
 		get root_path
 		assert_response :success
-		assert_select "title", "Home"
-		assert_select "h1", "Home"
+		assert_select "title", 	"Home"
+		assert_select "h1", 		"Home"
 	end
 
 	test "should get about" do
 		get about_path
 		assert_response :success
-		assert_select "title", "About"
-		assert_select "h1", "About"
+		assert_select "title", 	"About"
+		assert_select "h1", 		"About"
 	end
 
 	test "should get signup" do
 		get signup_path
 		assert_response :success
-		assert_select "title", "Sign Up"
-		assert_select "h1", "Sign Up"
+		assert_select "title", 	"Sign Up"
+		assert_select "h1", 		"Sign Up"
+	end
+
+	test "should get login" do
+		get login_path
+		assert_response :success
+		assert_select "title", 	"Log In"
+		assert_select "h1", 		"Log In"
 	end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,4 +14,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", login_path
 		assert_select "a[href=?]", logout_path, count: 0
 	end
+
+	test "layout links while logged in" do
+		log_in_as(@user)
+		get root_path
+		assert_select "a[href=?]", root_path
+		# assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", signup_path, count: 0
+		assert_select "a[href=?]", login_path,  count: 0
+		assert_select "a[href=?]", logout_path
+	end
 end
