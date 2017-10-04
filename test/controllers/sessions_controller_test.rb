acfd393d20d98bfecf2772b0c84c743193fb7932
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-04 09:44:57 +0200

    write tests for sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,9 +7,10 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 		@user2 	= users(:marion)
 	end
 
-	test "should redirect users index when not logged in" do
-		get users_path
-		assert redirected_to login_path
-	end
+	# test "should redirect users index when not logged in" do
+	# 	skip
+	# 	get users_path
+	# 	assert redirected_to login_path
+	# end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,4 +5,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		@user		= users(:manuel)
 		@user2	= users(:marion)
 	end
+
+	test "layout links while NOT logged in" do
+		get root_path
+		assert_select "a[href=?]", root_path
+		# assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", signup_path
+		assert_select "a[href=?]", login_path
+		assert_select "a[href=?]", logout_path, count: 0
+	end
 end
