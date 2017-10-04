3d948a312408c0f561a773a8514d7b099d2e8b0c
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-04 17:18:34 +0200

    generate Friendship model and set up basic relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 	validates :name, 	presence: true,
 										length: { maximum: 15 }
+
+	has_many :friendships, foreign_key: "friender", class_name: "Friendship"
+	has_many :friended,    through: :friendships
 end
