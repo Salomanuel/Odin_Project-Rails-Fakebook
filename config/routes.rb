570e9f7208b2f40cced7e0b4cc3abfae15f04052
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-04 09:58:44 +0200

    add links and routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	root 'base#index'
 	get '/about', 	to: 'base#about'
 	get '/signup', 	to: 'users#new'
+	get '/login',   to: 'sessions#new'
+	get '/logout',  to: 'sessions#destroy'
 	
 	resources :users, :only	=> [:index, :show, :create]
 
