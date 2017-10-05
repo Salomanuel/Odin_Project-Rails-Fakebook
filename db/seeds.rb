97cd78f2e519a3893e14c815efdadbc7485270fd
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-05 09:45:28 +0200

    seed the friendships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 User::delete_all
+Friendship::delete_all
 
 20.times do |n|
 	name = "User##{n}"
 	User.create!(name: name)
+end
+
+User.all.each do |user|
+	10.times do
+		user.friendships.new(friended_id: User.all.sample.id).save
+	end
 end
