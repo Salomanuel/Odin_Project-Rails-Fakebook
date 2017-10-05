2d32fe53ac65522597cd2cc188cd1357c8977006
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-05 09:45:09 +0200

    comment out the not-logged-in restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, except: [:new, :create]
+	# before_action :logged_in_user, except: [:new, :create]
 
 	def index
 		@users = User.all
